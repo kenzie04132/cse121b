13f4961d918b34d75c854e7de7a679fd4ad1725e
kenzie04132 <mackenzie04132@byui.edu>
2024-01-24 16:30:49 -0700

    W2 Programming Task - completed

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -3,24 +3,33 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
-
+let fullName = "Mackenzie Ash";
+let currentYear = "2024";
+let profilePicture = ("/w02-task/images/cse121b_me.png")
 
 /* Step 3 - Element Variables */
-
-
-
-
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector("img");
 
 /* Step 4 - Adding Content */
-
-
-
-
-
+nameElement.innerHTML = (`<strong>${fullName}</strong>`);
+yearElement.textContent = (`${currentYear}`);
+imageElement.setAttribute("src", `${profilePicture}`);
+imageElement.setAttribute("alt", `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
+let foods = ["Sushi", "Korean BBQ", "Mince Meat Pie", "Stromboli", "Mangoes"]
+
+const favoriteFood = "Käsespätzle"
+foods.push(favoriteFood);
+
+foodElement.innerHTML += `<br>${foods}`;
+foods.shift()
+foodElement.innerHTML += `<br>${foods}`;
+foods.pop()
+foodElement.innerHTML += `<br>${foods}`;
 
 
 
